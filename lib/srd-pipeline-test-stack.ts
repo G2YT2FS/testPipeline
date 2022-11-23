934774839736fc3c8272f5ebf99aa2764ad927fe
Seyedmohammad Paknejad <seyedmohammad.paknejad@porscheinformatik.com>
2022-11-23 10:11:51 +0100

    renew

--- lib/srd-pipeline-test-stack.ts
+++ lib/srd-pipeline-test-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import {
-  aws_codepipeline as ppl,
-  aws_codepipeline_actions as cpa,
-  SecretValue,
-  aws_iam as iam
+    aws_codepipeline as ppl,
+    aws_codepipeline_actions as cpa,
+    SecretValue,
+    aws_iam as iam
 } from 'aws-cdk-lib';
 
 import {Construct} from 'constructs';
@@ -11,51 +11,51 @@ import {BuildSpec, LinuxBuildImage, PipelineProject} from "aws-cdk-lib/aws-codeb
 
 export class SrdPipelineTestStack extends cdk.Stack {
 
-  protected defaultRole: iam.IRole;
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const pipeline = new ppl.Pipeline(this,'pipeline-test-mo', {
-      pipelineName:'Pipeline-test-mo',
-      crossAccountKeys:false
-    });
+        const pipeline = new ppl.Pipeline(this, 'pipeline-test-mo', {
+            pipelineName: 'Pipeline-test-mo',
+            crossAccountKeys: false
+        });
 
-    const sourceCode = new ppl.Artifact('sourceCode')
+        const sourceCode = new ppl.Artifact('sourceCode')
 
-    pipeline.addStage({
-      stageName:'source',
-      actions:[
-          new cpa.GitHubSourceAction({
-            owner:'SmPaknejad',
-            repo:'testPipeline',
-            branch:'main',
-            actionName:'PipelineSource',
-            oauthToken:SecretValue.secretsManager('test-mo-pipeline'),
-            output:sourceCode
-          })
-      ]
-    });
+        pipeline.addStage({
+            stageName: 'source',
+            actions: [
+                new cpa.GitHubSourceAction({
+                    owner: 'SmPaknejad',
+                    repo: 'testPipeline',
+                    branch: 'main',
+                    actionName: 'PipelineSource',
+                    oauthToken: SecretValue.secretsManager('test-mo-pipeline'),
+                    output: sourceCode
+                })
+            ]
+        });
 
-    const cdkBuildOutput = new ppl.Artifact('CDKBuildOutput')
+        const cdkBuildOutput = new ppl.Artifact('CDKBuildOutput');
 
-    pipeline.addStage({
-      stageName:'Build',
-      actions:[
-          new cpa.CodeBuildAction({
-            actionName:'Build',
-            role:this.defaultRole,
-            input:sourceCode,
-            outputs:[cdkBuildOutput],
-            project: new PipelineProject(this,'CodeBuildProject',{
-                environment: {
-                  buildImage: LinuxBuildImage.STANDARD_5_0
-                },
-              buildSpec:BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml')
+        pipeline.addStage({
+            stageName: 'Build',
+            actions: [
+                new cpa.CodeBuildAction({
+                    actionName: 'Build',
+                    //role: iam.Role.fromRoleArn(this,'mo-cross-account-build','arn:aws:iam::107711577954:role/test-mo-cross-account-pipeline-role'),
+                    input: sourceCode,
+                    outputs: [cdkBuildOutput],
+                    project: new PipelineProject(this, 'CodeBuildProject', {
+                        environment: {
+                            buildImage: LinuxBuildImage.STANDARD_5_0
+                        },
+                        buildSpec: BuildSpec.fromSourceFilename('build-specs/cdk-build-spec.yml')
 
-            })
-          })
-      ]
-    })
-
-  }
+                    })
+                })
+            ]
+        });
+    }
 }
+
+
